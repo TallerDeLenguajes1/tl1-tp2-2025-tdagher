61671dc975872dfadddbc4a1e54e5aac90e3f640
tdagher <102237680+tdagher@users.noreply.github.com>
2025-04-07 11:58:41 -0300

    actualizada funciones con aritmetica de punteros

--- tp2_4.c
+++ tp2_4.c
@@ -3,21 +3,19 @@
 #include <time.h>
 #include <string.h>
 
-struct compu
-{
+struct compu {
     int velocidad;
     int anio;
     int cantidad_nucleos;
     char *tipo_cpu;
 };
 
-void generarPCs(struct compu pcs[], int cantidad);
-void listarPCs(struct compu pcs[], int cantidad);
-void mostrarMasVieja(struct compu pcs[], int cantidad);
-void mostrarMasVeloz(struct compu pcs[], int cantidad);
+void generarPCs(struct compu *pcs, int cantidad);
+void listarPCs(struct compu *pcs, int cantidad);
+void mostrarMasVieja(struct compu *pcs, int cantidad);
+void mostrarMasVeloz(struct compu *pcs, int cantidad);
 
-int main()
-{
+int main() {
     srand(time(NULL));
 
     struct compu pcs[5];
@@ -35,52 +33,56 @@ int main()
     return 0;
 }
 
-void generarPCs(struct compu pcs[], int cantidad)
-{
+void generarPCs(struct compu *pcs, int cantidad) {
     char *tipos[6] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
-    for (int i = 0; i < cantidad; i++)
-    {
-        pcs[i].velocidad = rand() % 3 + 1;
-        pcs[i].anio = rand() % 10 + 2015;
-        pcs[i].cantidad_nucleos = rand() % 8 + 1;
-        pcs[i].tipo_cpu = tipos[rand() % 6];
+    for (int i = 0; i < cantidad; i++) {
+        (*(pcs + i)).velocidad = rand() % 3 + 1;
+        (*(pcs + i)).anio = rand() % 10 + 2015;
+        (*(pcs + i)).cantidad_nucleos = rand() % 8 + 1;
+
+        int indice_tipo = rand() % 6;
+        (*(pcs + i)).tipo_cpu = tipos[indice_tipo];
     }
 }
 
-void listarPCs(struct compu pcs[], int cantidad)
-{
-    for (int i = 0; i < cantidad; i++)
-    {
+void listarPCs(struct compu *pcs, int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
         printf("PC %d: Velocidad: %d GHz, Año: %d, Núcleos: %d, CPU: %s\n",
-               i + 1, pcs[i].velocidad, pcs[i].anio, pcs[i].cantidad_nucleos, pcs[i].tipo_cpu);
+               i + 1,
+               (*(pcs + i)).velocidad,
+               (*(pcs + i)).anio,
+               (*(pcs + i)).cantidad_nucleos,
+               (*(pcs + i)).tipo_cpu);
     }
 }
 
-void mostrarMasVieja(struct compu pcs[], int cantidad)
-{
+void mostrarMasVieja(struct compu *pcs, int cantidad) {
     int indice = 0;
-    for (int i = 1; i < cantidad; i++)
-    {
-        if (pcs[i].anio < pcs[indice].anio)
-        {
+    for (int i = 1; i < cantidad; i++) {
+        if ((*(pcs + i)).anio < (*(pcs + indice)).anio) {
             indice = i;
         }
     }
+
     printf("Velocidad: %d GHz, Año: %d, Núcleos: %d, CPU: %s\n",
-           pcs[indice].velocidad, pcs[indice].anio, pcs[indice].cantidad_nucleos, pcs[indice].tipo_cpu);
+           (*(pcs + indice)).velocidad,
+           (*(pcs + indice)).anio,
+           (*(pcs + indice)).cantidad_nucleos,
+           (*(pcs + indice)).tipo_cpu);
 }
 
-void mostrarMasVeloz(struct compu pcs[], int cantidad)
-{
+void mostrarMasVeloz(struct compu *pcs, int cantidad) {
     int indice = 0;
-    for (int i = 1; i < cantidad; i++)
-    {
-        if (pcs[i].velocidad > pcs[indice].velocidad)
-        {
+    for (int i = 1; i < cantidad; i++) {
+        if ((*(pcs + i)).velocidad > (*(pcs + indice)).velocidad) {
             indice = i;
         }
     }
+
     printf("Velocidad: %d GHz, Año: %d, Núcleos: %d, CPU: %s\n",
-           pcs[indice].velocidad, pcs[indice].anio, pcs[indice].cantidad_nucleos, pcs[indice].tipo_cpu);
+           (*(pcs + indice)).velocidad,
+           (*(pcs + indice)).anio,
+           (*(pcs + indice)).cantidad_nucleos,
+           (*(pcs + indice)).tipo_cpu);
 }
